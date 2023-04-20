bddee35f97f232536abc46a87e11a5852000ae24
GeovannyManga <geovannyx.casimirom@unac.edu.co>
2023-04-19 19:36:42 -0500

    kdvmv

--- api/config.js
+++ api/config.js
@@ -1,14 +1,14 @@
 //en este archivo se guardaran todas las variables de configuracion
 const config = {
     appConfig : {
-        port : process.env.APP_PORT||8080
+        port : process.env.APP_PORT||3000
     },
 
     dbConfig : {
         uri : process.env.URI,
         host : process.env.DB_HOST,
         dbName : process.env.DB_NAME,
-        dbPort : process.env.DB_PORT||8080
+        dbPort : process.env.DB_PORT||3000
     }
 }
 

3bbbbe778874e903f0faf89a8c93d869c1cc68ab
GeovannyManga <geovannyx.casimirom@unac.edu.co>
2023-04-19 19:39:54 -0500

    idcjdvij

--- api/src/app.js
+++ api/src/app.js
@@ -4,28 +4,19 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const morgan = require("morgan");
 const routes = require("./routes/index.js");
-
-//
-
 require("./db.js");
 
 const server = express();
 server.use(express.static("public"));
 server.name = "API";
 server.use(express.json());
-
-server.use(express.json());
-
 server.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
 server.use(bodyParser.json({ limit: "50mb" }));
 server.use(cookieParser());
 server.use(morgan("dev"));
+server.use(cors());
 server.use((req, res, next) => {
-  // res.header("Access-Control-Allow-Origin", "http://localhost:3000");
   res.header("Access-Control-Allow-Origin", "*");
-  // update to match the domain you will make the request from
-   res.header("Access-Control-Allow-Origin", "https://back20-production.up.railway.app/");
-
   res.header("Access-Control-Allow-Credentials", "true");
   res.header(
     "Access-Control-Allow-Headers",
@@ -37,16 +28,10 @@ server.use((req, res, next) => {
   );
   next();
 });
-
 server.use("/", routes);
-
-// Error catching endware.
 server.use((err, req, res, next) => {
-  // eslint-disable-line no-unused-vars
-  const status = err.status || 500;
-  const message = err.message || err;
-  console.error(err);
-  res.status(status).send(message);
+  console.error(err.stack);
+  res.status(500).send("Something broke!");
 });
 
 module.exports = server;
